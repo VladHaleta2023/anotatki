32f038bc35fc1176960ee0d9dc870e3185335229
VladHaleta2023 <vladhaleta2023@gmail.com>
2025-12-05 23:58:59 +0100

    Fix CSS file and update build

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -1,1 +1,19 @@
-globalThis.__BUILD_MANIFEST={polyfillFiles:["static/chunks/polyfills-42372ed130431b0a.js"],devFiles:[],ampDevFiles:[],lowPriorityFiles:[],rootMainFiles:["static/chunks/webpack-8d96c96156d3246d.js","static/chunks/4bd1b696-01da3eb66f93742b.js","static/chunks/684-08620eac2faaee0c.js","static/chunks/main-app-34bdcef6f6ccaa24.js"],rootMainFilesTree:{},pages:{"/_app":["static/chunks/webpack-8d96c96156d3246d.js","static/chunks/framework-f593a28cde54158e.js","static/chunks/main-ca9404338dcde335.js","static/chunks/pages/_app-da15c11dea942c36.js"],"/_error":["static/chunks/webpack-8d96c96156d3246d.js","static/chunks/framework-f593a28cde54158e.js","static/chunks/main-ca9404338dcde335.js","static/chunks/pages/_error-cc3f077a18ea1793.js"]},ampFirstPages:[]},globalThis.__BUILD_MANIFEST.lowPriorityFiles=["/static/"+process.env.__NEXT_BUILD_ID+"/_buildManifest.js",,"/static/"+process.env.__NEXT_BUILD_ID+"/_ssgManifest.js"];
+globalThis.__BUILD_MANIFEST = {
+  "polyfillFiles": [
+    "static/chunks/polyfills.js"
+  ],
+  "devFiles": [],
+  "ampDevFiles": [],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [],
+  "rootMainFilesTree": {},
+  "pages": {
+    "/_app": []
+  },
+  "ampFirstPages": []
+};
+globalThis.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
+
+];
